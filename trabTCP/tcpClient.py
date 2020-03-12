d8d0ccdc7d04c6cf41817b356894ba413500f211
angelocastro9 <castromatheus060@gmail.com>
2020-03-12 20:22:14 -0300

    Include headers for test

--- trabTCP/tcpClient.py
+++ trabTCP/tcpClient.py
@@ -29,6 +29,7 @@ try:
     if r.headers['Content-Type']:
         print(r.headers['Content-Type'])
 
+    print(r.headers)
     print(r.status_code)
     if r.status_code == 200:
         print("200 OK")

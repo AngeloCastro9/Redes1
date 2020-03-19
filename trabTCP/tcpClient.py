2c1cd45e609f63fe71daea228b2b3126333e6edd
angelocastro9 <castromatheus060@gmail.com>
2020-03-19 08:56:42 -0300

    All http response working - Socket

--- trabTCP/tcpClient.py
+++ trabTCP/tcpClient.py
@@ -1,6 +1,7 @@
 import socket
 from http import HTTPStatus
 import requests
+import re
 
 print("target host: ")
 target_host = input()
@@ -9,7 +10,7 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 
 try:
-    r = requests.get("https://"+target_host)
+
     client.connect((target_host,target_port))
     print("This's a valid server")
     print("Socket connected")
@@ -21,26 +22,25 @@ try:
 
     client.send(request.encode())
     
-    response = client.recv(4096)
+    response = client.recv(1024)
     http_response = repr(response)
     http_response_len = len(http_response)
     http_response_len_kb = (http_response_len/1000)
 
-    if r.headers['Content-Type']:
-        print(r.headers['Content-Type'])
-
-    print("HTTP Version: HTTP/1.1")
-    print(r.status_code)
     print(response[:8])
-
-    if r.status_code == 200:
+    print(response[9:12])
+    if response[9:12] == b'200':
         print("200 OK")
-    if r.status_code == 301:
+    if response[9:12] == b'301':
         print("301 Moved Permanently")
-    if r.status_code == 400:
+    if response[9:12] == b'400':
         print("400 BAD REQUEST")
-    if r.status_code == 404:
-        print("404 Not Found")
+    if response[9:12] == b'404':
+        print("404 Not Found")  
+
+    ct = response.decode().find("Content-Type")
+    if ct > 1:
+        print(response.decode()[ct:ct+25])
 
     print("[RECV] - length: %d" % http_response_len)
     print("Content length in Kb: %f" % http_response_len_kb)
